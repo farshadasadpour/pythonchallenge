79d957ba08a278f73c658efc22b6699b486d61e3
Farshad <asadpoor.f@gmail.com>
2022-01-24 12:13:33 +0330

    2.py

---
+++ 2.py
@@ -0,0 +1,41 @@
+# key
+alphabet = {
+        'a' : 'c', 
+        'b' : 'd', 
+        'c' : 'e', 
+        'd' : 'f', 
+        'e' : 'g', 
+        'f' : 'h', 
+        'g' : 'i', 
+        'h' : 'j', 
+        'i' : 'k', 
+        'j' : 'l', 
+        'k' : 'm', 
+        'l' : 'n', 
+        'm' : 'o', 
+        'n' : 'p', 
+        'o' : 'q', 
+        'p' : 'r', 
+        'q' : 's', 
+        'r' : 't', 
+        's' : 'u', 
+        't' : 'v', 
+        'u' : 'w', 
+        'v' : 'x', 
+        'w' : 'y', 
+        'x' : 'z', 
+        'y' : 'a', 
+        'z' : 'b'
+    }
+# data
+input_data = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+# soulotion 1
+# input_changed = list(input_data)
+# for item in range(len(input_changed)):
+#     input_changed[item] = alphabet.get(input_changed[item], input_changed[item])
+# print("".join(input_changed))
+
+# soulotion 2
+output = input_data.maketrans(alphabet)
+print(input_data.translate(output))
