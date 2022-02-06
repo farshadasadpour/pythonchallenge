1f301fce6a6e699e0440b1f630f5c059f2eb3848
Farshad <asadpoor.f@gmail.com>
2022-02-06 18:57:01 +0330

    challange 6

--- solution/6.py
+++ solution/6.py
@@ -6,14 +6,16 @@ import re
 
 
 archive = zipfile.ZipFile('/home/master/Downloads/channel.zip', 'r')
-next_number = re.findall(r"([0-9]+)",str(archive.read("90052.txt")))
+next_number = re.findall(r"([0-9]+)",archive.read("90052.txt").decode())
 next_number="".join(next_number)+".txt"
 
 for item in range(len(archive.namelist())):
-    next_number = re.findall(r"([0-9]+)",str(archive.read(next_number)))
+    print(archive.read(next_number).decode())
+    next_number = re.findall(r"([0-9]+)",archive.read(next_number).decode())
     if len(next_number) > 0:
         next_number="".join(next_number)+".txt"
         last_num = next_number
     else:
-        print(str(archive.read(last_num)))
+        print(archive.read(last_num).decode())
         break
+print(last_num)
