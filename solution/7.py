c95c6bceaad060697f770f9bb372f57d2338f92a
Farshad <asadpoor.f@gmail.com>
2022-02-19 17:39:08 +0330

    challeng 7

--- solution/7.py
+++ solution/7.py
@@ -10,10 +10,13 @@ img = Image.open(BytesIO(img_file))
 for i in range(img.height):
     r,g,b,a = img.getpixel((0,i))
     if r == g == b:
-        print(img.getpixel((0,i)),(0,i))
+        # print(img.getpixel((0,i)),(0,i))
         break
 
 list_pixcel= [img.getpixel((i,43)) for i in range(img.width)]
+
 list_pixcel =list_pixcel[::6]
+
 ords= [r for r,g,b,a in list_pixcel if r==g==b]
-char_list = "".join()
+
+print("".join(map(chr,ords)))
