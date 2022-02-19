dcd3e0cbcc1daca57c94ea913155e98c1ca8d59c
Farshad Asadpour <farshad_asadpour@tapin.ir>
2022-02-19 22:23:38 +0330

    challenge 7 passed

--- solution/7.py
+++ solution/7.py
@@ -10,13 +10,19 @@ img = Image.open(BytesIO(img_file))
 for i in range(img.height):
     r,g,b,a = img.getpixel((0,i))
     if r == g == b:
-        # print(img.getpixel((0,i)),(0,i))
+        # print(f"start: {img.getpixel((0,i)),(0,i)}")
         break
 
 list_pixcel= [img.getpixel((i,43)) for i in range(img.width)]
 
-list_pixcel =list_pixcel[::6]
+list_pixcel =list_pixcel[::7]
 
 ords= [r for r,g,b,a in list_pixcel if r==g==b]
 
 print("".join(map(chr,ords)))
+
+magic_list = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+
+print("".join(map(chr,magic_list)))
+
+# answer is integrity
